d8839ccec968396e2c3264fd44caa66f6d83213f
glarmer <hallrudfordlewis@gmail.com>
2026-03-11 01:25:02 +0000

    Temp fix airport console spam from error

--- Hide and PEAK/Hide and PEAK/src/Hide and PEAK/Patches/VoiceObscuranceFilterPatch.cs
+++ Hide and PEAK/Hide and PEAK/src/Hide and PEAK/Patches/VoiceObscuranceFilterPatch.cs
@@ -10,50 +10,57 @@ public class VoiceObscuranceFilterPatch
     [HarmonyPrefix]
     private static bool Prefix(VoiceObscuranceFilter __instance)
     {
-        if (!Plugin.ConfigurationHandler.ConfigSeekerVoice.Value) return true;
-        if (!HideAndSeekManager.Instance.IsGameActive) return true;
-        
-        var remoteCharacter = __instance.GetComponentInParent<Character>();
-        if (remoteCharacter == null || remoteCharacter.view == null) return true;
-        
-        var localCharacter = Character.localCharacter;
-        if (localCharacter == null || localCharacter.view == null) return true;
-        
-        if (remoteCharacter.name.Equals(localCharacter.name)) return true;
-        
-        remoteCharacter.view.Controller.CustomProperties.TryGetValue("Team", out var remoteTeamObj);
-        localCharacter.view.Controller.CustomProperties.TryGetValue("Team", out var localTeamObj);
-        
-        var remoteIsSeeker = remoteTeamObj != null && (Team)remoteTeamObj == Team.Seeker;
-        var localIsSeeker = localTeamObj != null && (Team)localTeamObj == Team.Seeker;
-        
-        if (remoteIsSeeker && localIsSeeker)
+        try
         {
-            if (__instance.lowPass != null) __instance.lowPass.cutoffFrequency = 22000f;
-        
-            if (__instance.echo != null)
+            if (!Plugin.ConfigurationHandler.ConfigSeekerVoice.Value) return true;
+            if (!HideAndSeekManager.Instance.IsGameActive) return true;
+
+            var remoteCharacter = __instance.GetComponentInParent<Character>();
+            if (remoteCharacter == null || remoteCharacter.view == null) return true;
+
+            var localCharacter = Character.localCharacter;
+            if (localCharacter == null || localCharacter.view == null) return true;
+
+            if (remoteCharacter.name.Equals(localCharacter.name)) return true;
+
+            remoteCharacter.view.Controller.CustomProperties.TryGetValue("Team", out var remoteTeamObj);
+            localCharacter.view.Controller.CustomProperties.TryGetValue("Team", out var localTeamObj);
+
+            var remoteIsSeeker = remoteTeamObj != null && (Team)remoteTeamObj == Team.Seeker;
+            var localIsSeeker = localTeamObj != null && (Team)localTeamObj == Team.Seeker;
+
+            if (remoteIsSeeker && localIsSeeker)
             {
-                __instance.echo.wetMix = 0f;
-                __instance.echo.dryMix = 1f;
-                __instance.echo.decayRatio = 0f;
-                __instance.echo.delay = 10f;
+                if (__instance.lowPass != null) __instance.lowPass.cutoffFrequency = 22000f;
+
+                if (__instance.echo != null)
+                {
+                    __instance.echo.wetMix = 0f;
+                    __instance.echo.dryMix = 1f;
+                    __instance.echo.decayRatio = 0f;
+                    __instance.echo.delay = 10f;
+                }
+
+                if (__instance.reverb != null) __instance.reverb.reverbLevel = 0f;
+
+                var source = __instance.GetComponent<AudioSource>();
+                if (source != null)
+                {
+                    source.spatialBlend = 0f;
+                    source.maxDistance = 10000f;
+                    source.minDistance = 0f;
+                    source.rolloffMode = AudioRolloffMode.Linear;
+                    source.dopplerLevel = 0f;
+                }
+
+                return false;
             }
-        
-            if (__instance.reverb != null) __instance.reverb.reverbLevel = 0f;
-            
-            var source = __instance.GetComponent<AudioSource>();
-            if (source != null)
-            {
-                source.spatialBlend = 0f;
-                source.maxDistance = 10000f;
-                source.minDistance = 0f;
-                source.rolloffMode = AudioRolloffMode.Linear;
-                source.dopplerLevel = 0f;
-            }
-        
-            return false;
         }
-        
+        catch
+        {
+            //Todo: this just stops the airport spam for now, this needs to be addressed properly later
+            return true;
+        }
         return true;
     }
 }
